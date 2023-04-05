21d88be3dd2aa31dc866812bf50663aebb4c3451
Kiryl Turau <71009434+KTurau@users.noreply.github.com>
2023-04-05 15:04:13 +0300

    Create sqlcode3-1.sql

---
+++ bootcamp/sqlcode3-1.sql
@@ -0,0 +1,45 @@
+/*
+CTE's
+*/
+
+
+WITH CTE_Employee AS 
+(
+SELECT 
+	FirstName,
+	LastName,
+	Gender,
+	Salary,
+	COUNT(Gender) OVER (PARTITION BY Gender) AS TotalGender,
+	AVG(Salary) OVER (Partition BY Gender) AS AvgSalary
+FROM 
+	EmployeeDemographics dem
+JOIN 
+	EmployeeSalary sal
+	ON 
+		dem.EmployeeID = sal.EmployeeID
+WHERE Salary > '45000'
+)
+
+SELECT 
+	FirstName,
+	AvgSalary
+FROM CTE_Employee;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
