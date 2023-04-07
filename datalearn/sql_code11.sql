c6efe4fb94df0ea1d547db9e6529fd3252b27c35
Kiryl Turau <71009434+KTurau@users.noreply.github.com>
2023-04-07 10:53:39 +0300

    Update sql_code11.sql

--- datalearn/sql_code11.sql
+++ datalearn/sql_code11.sql
@@ -1,5 +1,4 @@
 --Напишите запрос соединяющий таблицу orders и subscriptions
-
 SELECT
 	*
 FROM
@@ -7,8 +6,10 @@ FROM
 INNER JOIN
 	magazine.subscriptions AS s
 		ON o.subscription_id = s.subscription_id;
-		
---Дополните предыдущий запрос, и выведите только ту информацию, которая относится к подписке на журнал с названием "Fashion Magazine"
+	
+	
+--Дополните предыдущий запрос, и выведите только ту информацию, которая относится к подписке на журнал с названием 
+"Fashion Magazine"
 SELECT
 	*
 FROM
@@ -19,9 +20,9 @@ INNER JOIN
 WHERE
 	s.description = 'Fashion Magazine';
 	
+	
 --Напишите запрос, который выведет cтолбцы desription (название журнала), price_per_month (цену подписки за месяц)
 -- при условии, что длительность подписки = 1 год
-
 SELECT
 	s.description,
 	s.price_per_month
@@ -33,18 +34,20 @@ INNER JOIN
 WHERE
 	s.subscription_length = '12 months';
 
---Напишите запрос, который посчитает количество подписчиков на печатные издания
 
+--Напишите запрос, который посчитает количество подписчиков на печатные издания
 SELECT
 	COUNT(*) AS print_subscribers_count
 FROM
 	magazine.newspaper AS n;
+
 
 --Напишите запрос, который посчитает количество подписчиков на онлайн издания
 SELECT
 	COUNT(*) AS online_subscribers_count
 FROM
 	magazine.online AS o;
+
 
 --Напишите запрос, который найдет количество подписчиков, которые покупают печатные издания и онлайн издания
 SELECT
